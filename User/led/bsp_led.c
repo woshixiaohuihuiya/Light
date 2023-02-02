ee2ed3a1f18177406a95202f8e47c56761e0a5a9
woshixiaohuihuiya <95225114+woshixiaohuihuiya@users.noreply.github.com>
2023-02-02 14:16:23 +0800

    Update bsp_led.c
    修改编写日期

--- User/led/bsp_led.c
+++ User/led/bsp_led.c
@@ -3,14 +3,14 @@
   * @file    bsp_led.c
   * @author  fire
   * @version V1.0
-  * @date    2017-xx-xx
-  * @brief   ledÓ¦ÓÃº¯Êý½Ó¿Ú
+  * @date    2023-02-02
+  * @brief   ledåº”ç”¨å‡½æ•°æŽ¥å£
   ******************************************************************************
   * @attention
   *
-  * ÊµÑéÆ½Ì¨:Ò°»ð STM32 F103 ¿ª·¢°å  
-  * ÂÛÌ³    :http://www.firebbs.cn
-  * ÌÔ±¦    :http://firestm32.taobao.com
+  * å®žéªŒå¹³å°:é‡Žç« STM32 F103 å¼€å‘æ¿  
+  * è®ºå›    :http://www.firebbs.cn
+  * æ·˜å®    :http://firestm32.taobao.com
   *
   ******************************************************************************
   */
@@ -18,45 +18,45 @@
 #include "./led/bsp_led.h"   
 
  /**
-  * @brief  ³õÊ¼»¯¿ØÖÆLEDµÄIO
-  * @param  ÎÞ
-  * @retval ÎÞ
+  * @brief  åˆå§‹åŒ–æŽ§åˆ¶LEDçš„IO
+  * @param  æ— 
+  * @retval æ— 
   */
 void LED_GPIO_Config(void)
 {		
 		
-    /*¶¨ÒåÒ»¸öGPIO_InitTypeDefÀàÐÍµÄ½á¹¹Ìå*/
+    /*å®šä¹‰ä¸€ä¸ªGPIO_InitTypeDefç±»åž‹çš„ç»“æž„ä½“*/
     GPIO_InitTypeDef  GPIO_InitStruct;
 
-    /*¿ªÆôLEDÏà¹ØµÄGPIOÍâÉèÊ±ÖÓ*/
+    /*å¼€å¯LEDç›¸å…³çš„GPIOå¤–è®¾æ—¶é’Ÿ*/
     LED1_GPIO_CLK_ENABLE();
     LED2_GPIO_CLK_ENABLE();
     LED3_GPIO_CLK_ENABLE();
   
-    /*Ñ¡ÔñÒª¿ØÖÆµÄGPIOÒý½Å*/															   
+    /*é€‰æ‹©è¦æŽ§åˆ¶çš„GPIOå¼•è„š*/															   
     GPIO_InitStruct.Pin = LED1_PIN;	
 
-    /*ÉèÖÃÒý½ÅµÄÊä³öÀàÐÍÎªÍÆÍìÊä³ö*/
+    /*è®¾ç½®å¼•è„šçš„è¾“å‡ºç±»åž‹ä¸ºæŽ¨æŒ½è¾“å‡º*/
     GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;  
 
-    /*ÉèÖÃÒý½ÅÎªÉÏÀ­Ä£Ê½*/
+    /*è®¾ç½®å¼•è„šä¸ºä¸Šæ‹‰æ¨¡å¼*/
     GPIO_InitStruct.Pull  = GPIO_PULLUP;
 
-    /*ÉèÖÃÒý½ÅËÙÂÊÎª¸ßËÙ */   
+    /*è®¾ç½®å¼•è„šé€ŸçŽ‡ä¸ºé«˜é€Ÿ */   
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 
-    /*µ÷ÓÃ¿âº¯Êý£¬Ê¹ÓÃÉÏÃæÅäÖÃµÄGPIO_InitStructure³õÊ¼»¯GPIO*/
+    /*è°ƒç”¨åº“å‡½æ•°ï¼Œä½¿ç”¨ä¸Šé¢é…ç½®çš„GPIO_InitStructureåˆå§‹åŒ–GPIO*/
     HAL_GPIO_Init(LED1_GPIO_PORT, &GPIO_InitStruct);	
 
-    /*Ñ¡ÔñÒª¿ØÖÆµÄGPIOÒý½Å*/															   
+    /*é€‰æ‹©è¦æŽ§åˆ¶çš„GPIOå¼•è„š*/															   
     GPIO_InitStruct.Pin = LED2_PIN;	
     HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);	
 
-    /*Ñ¡ÔñÒª¿ØÖÆµÄGPIOÒý½Å*/															   
+    /*é€‰æ‹©è¦æŽ§åˆ¶çš„GPIOå¼•è„š*/															   
     GPIO_InitStruct.Pin = LED3_PIN;	
     HAL_GPIO_Init(LED3_GPIO_PORT, &GPIO_InitStruct);	
 
-    /*¹Ø±ÕRGBµÆ*/
+    /*å…³é—­RGBç¯*/
     LED_RGBOFF;
 }
 
